50f24b8606c167c6ae020881bb0fc2dfce7ec98e
mariam-ahmed10 <443204628@student.ksu.edu.sa>
2025-10-19 11:01:12 +0300

    Delete pages directory

--- pages/app_ids.dart
+++
@@ -1,8 +0,0 @@
-/// Central place for IDs used across the app.
-class AppIds {
-  /// Organizer doc id in 'Organizer' collection (from your screenshot)
-  static const organizerDocId = 'xtTXMnGpxp8jxIK9I5AE';
-
-  /// The value stored in Tournament.organizerID field (from your screenshot)
-  static const organizerKeyForTournaments = 'organizer_001';
-}
